0b36438f64695d089ad310755cbd239494d417aa
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-06-10 13:15:34 -0500

    Complete addKeyAndValue fcn

--- reduce.js
+++ reduce.js
@@ -56,7 +56,13 @@ Examples:
        ]
 */
 
-function addKeyAndValue(arr, key, value) {}
+function addKeyAndValue(arr, key, value) {
+  arr[0][key] = value;
+  return arr.reduce((acc, cur, idx) => {
+    cur[key] = value;
+    return arr;
+  });
+}
 
 /*
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
